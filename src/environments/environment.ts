a2ca0a5bdaea342d38399a40cc3de996113ba8f6
unknown <mahdi.dadvar013@gmail.com>
2026-09-01 15:21:34 +0430

    change url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://laravel-angular-new-booking.onrender.com',
+  apiBaseUrl: 'https://laravel-angular-new-booking.onrender.com/api',
 };
